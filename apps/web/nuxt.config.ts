7715f33dc7f09f821f6ff373cea8cdf6d5ab05bf
germondai <germondai@gmail.com>
2026-08-02 02:46:07 +0200

    fix(web): update canonical site URL

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
   },
 
   site: {
-    url: "https://trawl.dev",
+    url: "https://trawl.germondai.com",
     name: "TRAWL",
     description,
     defaultLocale: "en",
